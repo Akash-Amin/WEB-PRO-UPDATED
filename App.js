6e47528fb33ac426e50bf50e0f8a35277a6b865e
Akash-Amin <83532164+Akash-Amin@users.noreply.github.com>
2021-07-08 20:50:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './Component/Header/Header';
+
+
+
+
+
+
+function Appp() {
+  return (
+    <div className="app-div-class-name">
+      <Header/>
+    
+      
+       
+    </div> 
+  );
+}
+
+export default Appp;
